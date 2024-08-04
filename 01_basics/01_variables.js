224c046b61ed234db3a6c09cb49e23f036071628
AnkitRai26 <143981235+AnkitRai26@users.noreply.github.com>
2024-08-04 07:49:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12344
+let accountName = "Ankit"
+var accountMob = "8787878"
+accountCity = "Delhi"
+let accountState;
+
+accountName="Ankit Rai"
+accountMob="88877767"
+accountCity="new Delhi"
+/*
+prefer not to use var
+beacause of issue in block scope and function scope.
+*/
+
+console.table(
+    [accountId,accountName,accountMob,accountCity,accountState]
+)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treated all JS as a new version or JS
+
+// alert(3 + 3) we are using Node.js not browser
+// tc39 is offical documentation site and MDN is unoffical
+
+console.log( 3 + 3);
+                    // code should be readable
+console.lof("Ankit");
+
+// data type
+
+// 1-Number => 2*53
+// 2-BigInt => for larger number using in stock site ,reddit etc 
+// 3-string => " "
+// 4-Boolean => true/false
+// 5-null => standalone value 
+// 6-undefined => jo value define na ho 
+// 7-Symbol => unique like * ^ etc 
+// 8-Object 
+
+console.log(typeof "Ankit");
