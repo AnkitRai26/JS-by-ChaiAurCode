ecc1988916fc66e44f09d576fa4784090a2a47e8
AnkitRai26 <143981235+AnkitRai26@users.noreply.github.com>
2024-08-05 08:01:55 +0000

    datatypes

--- 01_basics/03_conversionconfusion.js
+++ 01_basics/03_conversionconfusion.js
@@ -1,6 +1,6 @@
 let score = "33abc"
-console.log(typeof score);
-console.log(typeof(score)); // both are same
+// console.log(typeof score);
+// console.log(typeof(score)); // both are same
 
 // if I neended in number 
 
@@ -18,13 +18,49 @@ let valueInNumber = Number(score)
 // if I need in boolean
 let isLoggedIn = 1
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 = true;  0=false;
 // " "=false; "Ankit"=true;
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ***************Operation*************
+
+
+let value = 3
+let negvalue = -value
+// console.log(value);
+
+// these are operation
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); 2^3
+// console.log(2 / 2);
+// console.log(2 % 2); for remainder
+
+let str1 = "hello"
+let str2 = " Ankit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 1);
+// console.log(1 + "1");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); string > number;
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+// console.log(num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //prefix and postflix study form Mdn
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// This expression checks if null is greater than 0.
+// In JavaScript, when comparing null with a number using the greater than (>) or less than (<) operators, null is converted to 0.
+// So, the comparison becomes 0 > 0, which is false.
+// 0 == 0 false
+// 0 >= 0 true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict rule
+
+console.log("2" === 2);
